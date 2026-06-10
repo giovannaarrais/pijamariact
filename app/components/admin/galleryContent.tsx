b78cb99a3c7d4105d92a2dfee09b2e5fdcee226c
giovannaarrais <giovannaarrais13@gmail.com>
2026-06-09 22:07:18 -0300

    :sparkles: feat: implementacao da listagem da galeria

--- app/components/admin/galleryContent.tsx
+++ app/components/admin/galleryContent.tsx
@@ -3,15 +3,17 @@
 import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '../ui/card';
 import { supabase } from '@/lib/supabaseStorage';
 import { Input } from '../ui/input';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { Button } from '../ui/button';
 import { refresh } from 'next/cache';
+import Image from 'next/image';
 
 function GalleryContent() {
     const [file, setFile] = useState<File | null>(null)
     const [errorMessage, setErrorMessage] = useState<string | null>(null)
     const [successMessage, setSuccessMessage] = useState<string | null>(null)
-
+    const [images, setImages] = useState<Array<any> | null>(null)
+    
     const updateImage = async( file: File ) => {
         if(!file) return;
 
@@ -34,6 +36,7 @@ function GalleryContent() {
                     setSuccessMessage(null)
                     setFile(null)
                 }, 3000)
+                await loadImages()
             } else {
                 setErrorMessage(result.message)
                 setTimeout(() => {
@@ -47,8 +50,45 @@ function GalleryContent() {
             setErrorMessage("Ocorreu um erro inesperado: " + error)
         }
     }
-    console.log(successMessage)
-    console.log(errorMessage)
+
+    const loadImages = async () => {
+        const { data: files, error } = await supabase.storage.from('produtos').list('pijamas')
+
+        if(error) {
+            console.log("ERRO AO CARREGAR IMAGENS", error)
+            return
+        }
+
+        if(files){
+            const newImages = files?.filter(file => file.name != ".emptyFolderPlaceholder").map((file) => {
+                const {data} = supabase.storage.from("produtos").getPublicUrl(`pijamas/${file.name}`)
+        
+                return{
+                    ...file,
+                    url: data.publicUrl
+                }
+            })
+        
+            if(newImages){
+                setImages(newImages)
+            } else {
+                setImages(null)
+            }
+        }
+
+    }
+
+    useEffect(() => {
+        loadImages()
+    }, [])
+
+
+    
+
+
+    console.log(images)
+
+
 
     return (
         <Card>
@@ -91,6 +131,27 @@ function GalleryContent() {
                     </Button>
                 </div>
                 {/* Lista de Imagens */}
+
+                <div className="grid grid-cols-4 gap-4">
+                    {images && images.length > 0 ? (
+                        images?.map((image) => (
+                        <Card key={image.id} className='py-3'>
+                            <CardContent className='px-3'>
+                                <Image
+                                 src={image.url} 
+                                 alt={image.name} 
+                                 width={300}
+                                 height={300}
+                                 className='rounded-lg'
+                                 />
+                                 <p className='text-gray-400 mt-2 text-center text-sm'>{image.name}</p>
+                            </CardContent>
+                        </Card>
+                    ))
+                    ) : (
+                        <p className=' text-gray-400 mt-2 text-center'>Nenhuma imagem encontrada</p>
+                    )}
+                </div>
             </CardContent>
         </Card>
     )
--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,14 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "urfrkttcaqxtrhtcwofl.supabase.co",
+      },
+    ],
+  }
 };
 
 export default nextConfig;
